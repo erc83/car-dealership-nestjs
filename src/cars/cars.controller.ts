242ea4f7eadcfdf60b8b3cd4f8a250e9b7f60eeb
erc83 <espinoza.eric@gmail.com>
2024-10-21 02:04:59 -0300

    add: CarsService and Inyection dependency

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,20 +1,24 @@
 import { Controller, Get, Param } from '@nestjs/common';
+import { CarsService } from './cars.service';
 
 @Controller('cars')           // contiene el controllador escucha la solicitud del cliente y emitir respuestas
 export class CarsController {
 
-    private cars = ['Toyota', 'Honda', 'Jeep']
+    //DI
+    constructor(
+        private readonly carsService: CarsService
+    ){}
 
     @Get()                    // necesita el metodo @Get, avisamos a nest que cuando este la solicitud responda
     getAllCars() {
-        return this.cars
+        return this.carsService.findAll()
     }
 
     @Get(':id')
     getCarById( @Param('id') id: string ) { //@Param('id') -  @Body() -  @Query() -  @Res()
         //console.log({ id })
 
-        return this.cars[id]
+        return this.carsService.findOneById( +id )
     }
 
 }
--- src/cars/cars.module.ts
+++ src/cars/cars.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common'
 import { CarsController } from './cars.controller'
+import { CarsService } from './cars.service';
 
 @Module({
     controllers: [CarsController],
+    providers: [CarsService],           // el en provider tiene que estar el CarsService
 })
 export class CarsModule {}
